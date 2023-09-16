c6c23eecb535efdbd57a3dc4e84896f8e875edfc
arsen <144203269+Iamarsenn@users.noreply.github.com>
2023-09-16 20:19:34 +0900

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,3 +1,7 @@
-hours=float(input("write your hours "))
-rate=float(input("write your rate "))
-print("ypur salary is ",hours*rate)
+decision=input("print c if you want to make farenheits \nprint f if you want to make celciums ")
+if decision=="c":
+    celciums=float(input("write here your celciums "))
+    print((celciums * 9/5) + 32,"farenheits")
+if decision=="f":
+    farenheits=float(input("write here your farenheits "))
+    print((farenheits-32) * 5/9,"celciums")
