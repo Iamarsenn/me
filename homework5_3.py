64549cede138b8250c1905d54397cce46b884d39
arsen <144203269+Iamarsenn@users.noreply.github.com>
2023-10-10 23:37:01 +0900

    Update homework5_3.py

--- homework5_3.py
+++ homework5_3.py
@@ -1,6 +1,10 @@
-# inp = input("Enter a sentence ")
-#
-# num_c = sum(c.isdigit() for c in inp)
-# up_c = sum(c.isupper() for c in inp)
-# low_c = sum(c.islower() for c in inp)
-# other_c = len(inp) - (num_c + up_c + low_c)
+inp = input("Enter a sentence ")
+
+num_c = sum(c.isdigit() for c in inp)
+up_c = sum(c.isupper() for c in inp)
+low_c = sum(c.islower() for c in inp)
+other_c = len(inp) - (num_c + up_c + low_c)
+print("number of numbers:", num_c)
+print("number of uppercase letters:", up_c)
+print("number of lowercase letters:", low_c)
+print("number of other characters:", other_c)
