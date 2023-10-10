f184150b15f7f919a4886885629ef81170378631
arsen <144203269+Iamarsenn@users.noreply.github.com>
2023-10-10 23:36:20 +0900

    Create homework5_3.py

---
+++ homework5_3.py
@@ -0,0 +1,6 @@
+# inp = input("Enter a sentence ")
+#
+# num_c = sum(c.isdigit() for c in inp)
+# up_c = sum(c.isupper() for c in inp)
+# low_c = sum(c.islower() for c in inp)
+# other_c = len(inp) - (num_c + up_c + low_c)
